43bc4c704e916ce0c17926ba90d4f0c42b8f60e1
adem-emre <79714460+adem-emre@users.noreply.github.com>
2021-12-13 11:27:22 +0300

    const file edited

--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -3,4 +3,3 @@ import 'dart:ui';
 const whatsappMainColor = Color(0xff075E54);
 
 
-    const zuckerbergImage="https://upload.wikimedia.org/wikipedia/commons/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg";
